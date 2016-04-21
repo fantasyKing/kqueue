095d3078e2da0b63d9ec28db48d8f7a6f1e49aac
panlilu <panlilu@gmail.com>
2016-04-21 15:10:24 +0800

    add index

---
+++ dist/index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _Kconsumer = require('./Kconsumer');
+
+var _Kconsumer2 = _interopRequireDefault(_Kconsumer);
+
+var _Khub = require('./Khub');
+
+var _Khub2 = _interopRequireDefault(_Khub);
+
+var _Kproducer = require('./Kproducer');
+
+var _Kproducer2 = _interopRequireDefault(_Kproducer);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = {
+  Kconsumer: _Kconsumer2.default,
+  Khub: _Khub2.default,
+  Kproducer: _Kproducer2.default
+};
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import Kconsumer from './Kconsumer';
+import Khub from './Khub';
+import Kproducer from './Kproducer';
+
+export default {
+  Kconsumer,
+  Khub,
+  Kproducer
+}
